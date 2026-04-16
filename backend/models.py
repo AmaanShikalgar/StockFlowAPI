7fb1c78d049f8f0753a86cbfe0418102c6d56ae3
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-04-16 17:02:19 +0530

    Fix delete endpoint and finalize CRUD API

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: int
+    name: str
+    description : str
+    price : float
+    quantity : int
+    
+    # use if no base model    
+    # def __init__(self, id: int, name: str, descriptiom : str, price : float, quantity : int):
+    #     self.id = id
+    #     self.name = name
+    #     self.description = descriptiom
+    #     self.price = price
+    #     self.quantity = quantity
+    
+    
