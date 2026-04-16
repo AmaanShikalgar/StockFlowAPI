5a5fd4b00677d4cd82dd7e3a9aa62f9e9a2b9c03
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-04-16 17:14:06 +0530

    Final clean structure

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI , Depends
 from fastapi.middleware.cors import CORSMiddleware
-from models import Product
-from database import session, engine
-import database_models as database_models
+from backend.models import Product
+from backend.database import session, engine
+import backend.database_models as database_models
 from sqlalchemy.orm import Session
 
 app = FastAPI()
