d113971d919bfe00a56ed8048909566e5aa486bc
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-04-19 18:16:33 +0530

    fixed jwt

--- backend/main.py
+++ backend/main.py
@@ -62,7 +62,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://stock-flow-api.vercel.app"
+        "https://stock-flow-pkkkh8trq-amaan-shikalgars-projects.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
